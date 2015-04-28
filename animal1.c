963c6dadeabbe0ef3419a238494b4b32715344de
ADanilychevJr <danilychev@berkeley.edu>
2015-04-28 14:47:45 -0700

    Finished init1

--- animal1.c
+++ animal1.c
@@ -10,5 +10,17 @@ struct positionStruct {
 
 TreeType InitTree () {
     TreeType tree = malloc(sizeof(TreeType));
+    int i;
+    for (i; i < MAXNUMQS; i++){
+        char * question = calloc(MAXSTRLEN,sizeof(char));
+        (tree->nodes)[i] = question;
+    }
+    strcpy(tree->nodes[0],"Is it furry?");
+    strcpy(tree->nodes[1],"Does it meow?");
+    strcpy(tree->nodes[2],"Does it have tusks?");
+    strcpy(tree->nodes[(2*1)+1],"a cat");
+    strcpy(tree->nodes[(2*1)+2],"a dog");
+    strcpy(tree->nodes[(2*2)+1],"a rhino");
+    strcpy(tree->nodes[(2*2)+2],"a gecko");
     return tree;
 }
