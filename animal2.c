2bc3009ebd1a6605ad615f357740c1072994598b
ADanilychevJr <danilychev@berkeley.edu>
2015-04-27 18:06:11 -0700

    Added init tree

--- animal2.c
+++ animal2.c
@@ -8,3 +8,22 @@ struct treeStruct {
 struct positionStruct {
     struct treeStruct* node;
 };
+
+
+TreeType InitTree () {
+    TreeType tree = calloc(MAXNUMQS,sizeof(char*));
+    int i;
+    for (i; i < MAXNUMQS; i++){
+        char * question = calloc(MAXSTRLEN,sizeof(char));
+        tree[i] = question;
+    }
+    strcpy(tree[0],"Is it furry?");
+    strcpy(tree[1],"Does it meow?");
+    strcpy(tree[2],"Does it have tusks?");
+    strcpy(tree[(2*1)+1],"a cat");
+    strcpy(tree[(2*1)+2],"a dog");
+    strcpy(tree[(2*2)+1],"a rhino");
+    strcpy(tree[(2*2)+2],"a gecko");
+    //PrintTree(tree);
+    return tree;
+}
