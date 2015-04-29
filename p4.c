29d8d4f7d6036f01a42d3aa132003eede1e6475e
ADanilychevJr <danilychev@berkeley.edu>
2015-04-28 20:03:21 -0700

    Lazy commit

--- p4.c
+++ p4.c
@@ -39,6 +39,7 @@ int main (int argc, char *argv[])
     }
     
     tree = InitTree (treefile);
+    PrintTree(tree);
     printf("%s", "Think of an animal. I will try to guess what it is.\n"
 		 "Please answer my questions with yes or no.\n");
     while (true) {
